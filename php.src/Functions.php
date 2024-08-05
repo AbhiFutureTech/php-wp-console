453afc933a4cafa66f12401a8abacb4f2ae2338c
Abhijit Patil <157373320+AbhiFutureTech@users.noreply.github.com>
2024-08-05 18:33:14 +0530

    Add files via upload

---
+++ php.src/Functions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * WP PHP Console
+ *
+ * This source file is subject to the GNU General Public License v3.0
+ * that is bundled with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @author    Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
+ * @copyright Copyright (c) 2014-2020 Fulvio Notarstefano
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+defined( 'ABSPATH' ) or exit;
+
+
+/**
+ * Gets the WP PHP Console instance.
+ *
+ * @since 1.5.4
+ *
+ * @return WP_PHP_Console\Plugin
+ */
+function wp_php_console() {
+
+	return new \WP_PHP_Console\Plugin();
+}
